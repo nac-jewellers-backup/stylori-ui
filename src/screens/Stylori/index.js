7ea8335321a5bf4d87b18d726358d8510695977d
akaDPR <prakash@crayond.co>
2019-07-08 19:30:49 +0530

    stylori header

--- src/screens/Stylori/index.js
+++ src/screens/Stylori/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header/header'
 
 export default class Stylori extends Component {
     render() {
         return (
             <div>
-                Test <button onClick={() => { this.props.history.push('/') }} >Home</button>
+                {/* Test <button onClick={() => { this.props.history.push('/') }} >Home</button> */}
+                <Header/>
             </div>
         )
     }
