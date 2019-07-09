2d9e214dee6c8f4da175d14f1979a51c31169249
akaDPR <prakash@crayond.co>
2019-07-09 14:12:50 +0530

    ui header and navigation laptop view

--- src/screens/Stylori/index.js
+++ src/screens/Stylori/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/header'
+import Filter from '../../components/Filter/filter'
 
 export default class Stylori extends Component {
     render() {
@@ -7,6 +8,7 @@ export default class Stylori extends Component {
             <div>
                 {/* Test <button onClick={() => { this.props.history.push('/') }} >Home</button> */}
                 <Header/>
+                {/* <Filter/> */}
             </div>
         )
     }
