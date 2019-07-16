3c37669463a4af5a7622ab4afb548905567ab6c2
crayondmakk <afroze@crayond.co>
2019-07-16 23:56:18 +0530

    online offline check

---
+++ src/screens/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Loading = (
+    <div>
+        Loading Page
+    </div>
+)
+
+export default Loading;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,5 @@
 import Stylori from './Stylori';
 
+export * from './Loading';
+
 export { Stylori }
