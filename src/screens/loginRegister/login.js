0aac5bd339dbc284a663ad3967bed1a31d43601a
akaDPR <prakash@crayond.co>
2019-08-23 14:29:23 +0530

    ui

---
+++ src/screens/loginRegister/login.js
@@ -0,0 +1,19 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+import './loginRegister.css'
+
+class Login extends React.Component {
+
+
+    render() {
+        return (
+            <Container>
+                <div className='pt-sm'>
+                    <h5 className='title'>  New user registration  </h5>
+                    
+                </div>
+            </Container>
+        )
+    }
+}
+export default Login;
---
+++ src/screens/loginRegister/register.js
@@ -0,0 +1,16 @@
+import { } from '@material-ui/core';
+import React from 'react';
+import './product-images.css'
+
+class Register extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Register;
