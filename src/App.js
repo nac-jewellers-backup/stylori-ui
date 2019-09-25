b6b9ad9da23019e657db4650d8fc6aa6f4862929
crayondmakk <afroze@crayond.co>
2019-09-25 17:10:42 +0530

    alias resolve feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { NetworkProvider, GlobalProvider } from './context';
+import { NetworkProvider, GlobalProvider } from 'context';
 import './index.css'
-import Loading from './screens/Loading';
+import Loading from 'screens/Loading';
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 let outerTheme = createMuiTheme(require('./theme.json'));
 let outerThemes = responsiveFontSizes(outerTheme);
-const RouterApp = React.lazy(() => import('./router'));
+const RouterApp = React.lazy(() => import('router'));
 
 function App() {
   return (
