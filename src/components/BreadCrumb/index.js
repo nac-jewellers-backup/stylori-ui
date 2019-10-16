49de7d9493cf17509e2650615a57d1ba448044ae
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2019-10-16 21:23:43 +0530

    Updated the cart and checkout page functionality

--- src/components/BreadCrumb/index.js
+++ src/components/BreadCrumb/index.js
@@ -2,55 +2,50 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Paper, Breadcrumbs, Link, Container, Grid, Typography } from '@material-ui/core/';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-
 export default function CustomSeparator(props) {
-  let path = window.location.pathname.split('/').pop();
-  let seperators = path === 'cart' || path === "checkout" ?
-    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAQAAAC0NkA6AAAAAmJLR0QA/4ePzL8AAABTSURBVFjD7dfBDYAgFARRqkBCi1ouEa1mbMJPIMw08JK9bUpm9lOclGjiAnowQ6YBL1VGRkZGZjKm0IGbvDbCQQOewLkkJCQkNicGXYchJ8hs8z7oGPzdOrNn/QAAAABJRU5ErkJggg==" className={props.arrowicon} />
-    : <NavigateNextIcon />;
-  return (
-    <div className={props.className}>
-      <Paper elevation={0} className={props.classsubhed}>
-        <Grid container>
-          <Grid item lg={7}>
-            <Breadcrumbs separator={seperators}>
-              {
-                props.data.map(data => (
-                  <Link color="inherit" onClick={data} style={{ fontSize: "14px" }} className={props.list}>
-                    {data}
-                  </Link>
-                ))
-              }
-            </Breadcrumbs>
-          </Grid>
-          <Grid item lg={5}>
-            {props.subdata ?
-              <div>
-
-                <Grid container style={{ height: "32px", lineHeight: "32px" }}>
-                  {
-                    props.subdata.map(subdata => (
-                      <>
-                        {/* <Grid item xs={1} style={{ marginTop: "5px" }}> */}
-                        <div style={{marginTop:"0.5%",marginRight:"5px"}}>
-                        <img src={subdata.icon} style={{ width: "25px", height: "25px" }} />
-                        </div>
-                        {/* </Grid> */}
-                        <Grid item xs={2} style={{ float: "left", lineHeight: "14px" }}>
-                          <span style={{ fontSize: "13px" }}> {subdata.name}</span>
-                        </Grid>
-                      </>
-                    ))
-                  }
-                </Grid>
-
-              </div>
-
-              : ""}
-          </Grid>
-        </Grid>
-
-      </Paper>
-    </div>
-  );
+ let path = window.location.pathname.split('/').pop();
+ let seperators = path === 'cart' || path === "checkout" ?
+   <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAQAAAC0NkA6AAAAAmJLR0QA/4ePzL8AAABTSURBVFjD7dfBDYAgFARRqkBCi1ouEa1mbMJPIMw08JK9bUpm9lOclGjiAnowQ6YBL1VGRkZGZjKm0IGbvDbCQQOewLkkJCQkNicGXYchJ8hs8z7oGPzdOrNn/QAAAABJRU5ErkJggg==" className={props.arrowicon} />
+   : <NavigateNextIcon />;
+ return (
+   <div className={props.className}>
+     <Paper elevation={0} className={props.classsubhed}>
+       <Grid container>
+         <Grid item lg={7}>
+           <Breadcrumbs separator={seperators}>
+             {
+               props.data.map(data => (
+                 <Link color="inherit" onClick={data} style={{ fontSize: "14px" }} className={props.list}>
+                   {data}
+                 </Link>
+               ))
+             }
+           </Breadcrumbs>
+         </Grid>
+         <Grid item lg={5}>
+           {props.subdata ?
+             <div>
+               <Grid container style={{ height: "32px", lineHeight: "32px" }}>
+                 {
+                   props.subdata.map(subdata => (
+                     <>
+                       {/* <Grid item xs={1} style={{ marginTop: "5px" }}> */}
+                       <div style={{marginTop:"0.5%",marginRight:"5px"}}>
+                       <img src={subdata.icon} style={{ width: "25px", height: "25px" }} />
+                       </div>
+                       {/* </Grid> */}
+                       <Grid item xs={2} style={{ float: "left", lineHeight: "14px" }}>
+                         <span style={{ fontSize: "13px" }}> {subdata.name}</span>
+                       </Grid>
+                     </>
+                   ))
+                 }
+               </Grid>
+             </div>
+             : ""}
+         </Grid>
+       </Grid>
+     </Paper>
+   </div>
+ );
 }
--- src/components/InputComponents/Select/Select.js
+++ src/components/InputComponents/Select/Select.js
@@ -6,66 +6,60 @@ import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import { Select, OutlinedInput } from "@material-ui/core";
 import PropTypes from 'prop-types';
-
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: "flex",
-        flexWrap: "wrap"
-    },
-    formControl: {
-        margin: theme.spacing(1),
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2)
-    }
+   root: {
+       display: "flex",
+       flexWrap: "wrap"
+   },
+   formControl: {
+       margin: theme.spacing(1),
+   },
+   selectEmpty: {
+       marginTop: theme.spacing(2)
+   }
 }));
-
 export default function SimpleSelect(props) {
-    const classes = useStyles();
-    const [values, setValues] = React.useState({
-        age: null,
-        helperText: false
-    });
-
-    React.useEffect(() => {
-        // console.info('EVENT', values.age, Boolean((values.age != null && values.age !== "")));
-    }, [values.age])
-
-    function handleChange(event) {
-        setValues({ [event.target.name]: event.target.value, helperText: !Boolean((event.target.value != null && event.target.value !== "")) })
-    }
-
-    return (
-        <form className={classes.root} autoComplete="off">
-            {/* <FormControl required className={classes.formControl} style={{ width: "100%" }}>
-                {props.name ?
-                    <InputLabel htmlFor="outlined-age-native-simple">{props.name}</InputLabel> : ""
-                } */}
-            <Select
-                style={{ width: "100%" }}
-                displayEmpty
-                renderValue={value => props.name ? props.name : "Select"}
-                onChange={handleChange}
-                // name={props.name ? props.name : ""}
-                input={<OutlinedInput id="outlined-age-native-simple" />}
-                inputProps={{ id: "outlined-age-native-simple", "aria-required": true }}
-                MenuProps={{ "aria-required": true }}
-                className={classes.selectEmpty}
-                variant="outlined"
-            >
-                <MenuItem value="">
-                    <em>None</em>
-                </MenuItem>
-                {props.selectData.map(data => (
-                    <MenuItem value={data}>{data}</MenuItem>
-                ))}
-            </Select>
-            {values.helperText && <FormHelperText><b style={{ color: 'red' }}>Required</b></FormHelperText>}
-            {/* </FormControl> */}
-        </form>
-    );
+   const classes = useStyles();
+   const [values, setValues] = React.useState({
+       age: null,
+       helperText: false
+   });
+   React.useEffect(() => {
+       // console.info('EVENT', values.age, Boolean((values.age != null && values.age !== "")));
+   }, [values.age])
+   function handleChange(event) {
+       debugger
+       setValues({ [event.target.name]: event.target.value, helperText: !Boolean((event.target.value != null && event.target.value !== "")) })
+   }
+   return (
+       <form className={classes.root} autoComplete="off">
+           {/* <FormControl required className={classes.formControl} style={{ width: "100%" }}>
+               {props.name ?
+                   <InputLabel htmlFor="outlined-age-native-simple">{props.name}</InputLabel> : ""
+               } */}
+           <Select
+               style={{ width: "100%" }}
+               displayEmpty
+               renderValue={value => values[props.name] ? values[props.name] : props.name ? props.name : "Select"}
+               onChange={(e) => handleChange(e)}
+               value={values[props.name]}
+               name={props.name ? props.name : "select"}
+               input={<OutlinedInput id="outlined-age-native-simple" />}
+               inputProps={{ id: "outlined-age-native-simple", "aria-required": true }}
+               MenuProps={{ "aria-required": true }}
+               className={classes.selectEmpty}
+               variant="outlined"
+           >
+               {props.selectData.map(data => (
+                   <MenuItem value={data}>{data}</MenuItem>
+               ))}
+           </Select>
+           {values.helperText && <FormHelperText><b style={{ color: 'red' }}>Required</b></FormHelperText>}
+           {/* </FormControl> */}
+       </form>
+   );
 }
 SimpleSelect.propTypes = {
-    handleChange: PropTypes.func,
-    selectData: PropTypes.object.isRequired
+   handleChange: PropTypes.func,
+   selectData: PropTypes.object.isRequired
 }
