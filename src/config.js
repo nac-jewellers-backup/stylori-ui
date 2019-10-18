8ae63703a0b6a34d61400e4dc29270259877c41a
akaDPR <prakash@crayond.co>
2019-10-18 13:51:32 +0530

    url change

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://api.stylori.net"
 export const HOME_PAGE_URL = ""
-export const CDN_URL = "https://assets.stylori.net"
+export const CDN_URL = "https://assets.stylori.net/"
