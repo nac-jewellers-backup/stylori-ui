c6986cd0307b48c8cfe60fd4c5546d5b157dec5e
akaDPR <prakash@crayond.co>
2019-11-11 20:46:12 +0530

    checkout bugs

--- build/precache-manifest.6b65fcea232704106a6a23da34f55726.js
+++ build/precache-manifest.6b65fcea232704106a6a23da34f55726.js
@@ -1,51 +1,51 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88e8207b2f837d142b26",
+    "revision": "45c68df64ae3a998b755",
     "url": "/0.css"
   },
   {
-    "revision": "53416b6ba53e37330522",
+    "revision": "12d8c7a10cb41e7ccac0",
     "url": "/2.css"
   },
   {
-    "revision": "276c0f89891a829c0d42",
+    "revision": "b7a7940724912a151c66",
     "url": "/4.css"
   },
   {
-    "revision": "6761b2c548a54e7ac12d1f8ea654bc89",
+    "revision": "b0de052b99d1ce384c2066a1ab69d36c",
     "url": "/index.html"
   },
   {
-    "revision": "53416b6ba53e37330522",
+    "revision": "12d8c7a10cb41e7ccac0",
     "url": "/static/css/2.18585a14.chunk.css"
   },
   {
-    "revision": "276c0f89891a829c0d42",
+    "revision": "b7a7940724912a151c66",
     "url": "/static/css/4.f0a8dbea.chunk.css"
   },
   {
-    "revision": "88e8207b2f837d142b26",
+    "revision": "45c68df64ae3a998b755",
     "url": "/static/css/main.740a324f.chunk.css"
   },
   {
-    "revision": "53416b6ba53e37330522",
-    "url": "/static/js/2.67ad56b3.chunk.js"
+    "revision": "12d8c7a10cb41e7ccac0",
+    "url": "/static/js/2.22269a85.chunk.js"
   },
   {
     "revision": "6f7649cc20863ebed28e",
     "url": "/static/js/3.a73995e2.chunk.js"
   },
   {
-    "revision": "276c0f89891a829c0d42",
-    "url": "/static/js/4.a5c5c216.chunk.js"
+    "revision": "b7a7940724912a151c66",
+    "url": "/static/js/4.bdcb1119.chunk.js"
   },
   {
-    "revision": "88e8207b2f837d142b26",
-    "url": "/static/js/main.3d9c53b9.chunk.js"
+    "revision": "45c68df64ae3a998b755",
+    "url": "/static/js/main.72dbc1f3.chunk.js"
   },
   {
-    "revision": "a6c8a06097f5792edd95",
-    "url": "/static/js/runtime~main.c3ab9aca.js"
+    "revision": "057ba11d72bf49abbdd7",
+    "url": "/static/js/runtime~main.85b25745.js"
   },
   {
     "revision": "d20d098dccc3deb830808f97fb14f3ca",
