3ab6b7bdb2572bd7eae620503223697f5efce932
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2019-11-19 19:46:34 +0530

    url product detail fixes

---
+++ build/precache-manifest.5513f83350ce9f4574297d67bbfd2c54.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c13ed8c826029c8c2f",
+    "url": "/0.css"
+  },
+  {
+    "revision": "5f21fc9f571ccf3cea7c",
+    "url": "/2.css"
+  },
+  {
+    "revision": "ee11bb04c0e54a7720da",
+    "url": "/4.css"
+  },
+  {
+    "revision": "105e2a9a46eb9dce5667ac30f916342e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f21fc9f571ccf3cea7c",
+    "url": "/static/css/2.5f966f18.chunk.css"
+  },
+  {
+    "revision": "ee11bb04c0e54a7720da",
+    "url": "/static/css/4.511ba922.chunk.css"
+  },
+  {
+    "revision": "b7c13ed8c826029c8c2f",
+    "url": "/static/css/main.740a324f.chunk.css"
+  },
+  {
+    "revision": "5f21fc9f571ccf3cea7c",
+    "url": "/static/js/2.609e298d.chunk.js"
+  },
+  {
+    "revision": "1c4ae20cdd0e33e228b7",
+    "url": "/static/js/3.83f24e01.chunk.js"
+  },
+  {
+    "revision": "ee11bb04c0e54a7720da",
+    "url": "/static/js/4.517522ac.chunk.js"
+  },
+  {
+    "revision": "b7c13ed8c826029c8c2f",
+    "url": "/static/js/main.6e760c2a.chunk.js"
+  },
+  {
+    "revision": "321987578a13ea010c2c",
+    "url": "/static/js/runtime~main.e553d52a.js"
+  },
+  {
+    "revision": "d20d098dccc3deb830808f97fb14f3ca",
+    "url": "/static/media/Diners-Club.d20d098d.jpg"
+  },
+  {
+    "revision": "c3f7641ef11bd405cfe12138f25f089f",
+    "url": "/static/media/cartoonFooter.c3f7641e.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0a848483a9d0cc238c3b584e1260e3ec",
+    "url": "/static/media/visa.0a848483.jpg"
+  }
+]);
