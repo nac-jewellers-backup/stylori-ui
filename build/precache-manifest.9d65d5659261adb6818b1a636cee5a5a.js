b15e84d8fe88c3a62f444a61259f37da109df098
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2019-11-25 15:27:36 +0530

    updated the homepage package.json file

---
+++ build/precache-manifest.9d65d5659261adb6818b1a636cee5a5a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273f0054c239ad38e504",
+    "url": "/0.css"
+  },
+  {
+    "revision": "6cabf21fa4da94cfe630",
+    "url": "/2.css"
+  },
+  {
+    "revision": "934b514076de6885c9f9",
+    "url": "/3.css"
+  },
+  {
+    "revision": "667ada9973d9e88a66c7d71c9ca709be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cabf21fa4da94cfe630",
+    "url": "/static/css/2.5f966f18.chunk.css"
+  },
+  {
+    "revision": "934b514076de6885c9f9",
+    "url": "/static/css/3.61e4ee32.chunk.css"
+  },
+  {
+    "revision": "273f0054c239ad38e504",
+    "url": "/static/css/main.740a324f.chunk.css"
+  },
+  {
+    "revision": "6cabf21fa4da94cfe630",
+    "url": "/static/js/2.d196d163.chunk.js"
+  },
+  {
+    "revision": "934b514076de6885c9f9",
+    "url": "/static/js/3.500b8458.chunk.js"
+  },
+  {
+    "revision": "a8547f777f48da134b31",
+    "url": "/static/js/4.ba9fa20f.chunk.js"
+  },
+  {
+    "revision": "273f0054c239ad38e504",
+    "url": "/static/js/main.74e1f455.chunk.js"
+  },
+  {
+    "revision": "292c26064c9573bdf4b8",
+    "url": "/static/js/runtime~main.5e1b6201.js"
+  },
+  {
+    "revision": "d20d098dccc3deb830808f97fb14f3ca",
+    "url": "/static/media/Diners-Club.d20d098d.jpg"
+  },
+  {
+    "revision": "92e2d23a1a2929823738532362c05eea",
+    "url": "/static/media/FlowerDimon.92e2d23a.jpg"
+  },
+  {
+    "revision": "c3f7641ef11bd405cfe12138f25f089f",
+    "url": "/static/media/cartoonFooter.c3f7641e.png"
+  },
+  {
+    "revision": "1d4ab251e668bd11b62b30615bd88e78",
+    "url": "/static/media/closeheart.1d4ab251.jpg"
+  },
+  {
+    "revision": "965dabcd31d04c27b319f732b7aae019",
+    "url": "/static/media/nosepin.965dabcd.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0a848483a9d0cc238c3b584e1260e3ec",
+    "url": "/static/media/visa.0a848483.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ce61ff2045da068dfa00c79941ebd711.js"
+  "/precache-manifest.9d65d5659261adb6818b1a636cee5a5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
