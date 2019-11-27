c13d29cc1d2e3abbb23639c97e59f019e74eb0e4
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2019-11-27 20:30:25 +0530

    filters url fixed and filters data mapped

---
+++ build/precache-manifest.d360680475dd4d6fd193433d499b0576.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf20692c1739dbbb9b6e",
+    "url": "/0.css"
+  },
+  {
+    "revision": "79e3ff328b79b5bd841c",
+    "url": "/2.css"
+  },
+  {
+    "revision": "31174416e907dad5f29e",
+    "url": "/3.css"
+  },
+  {
+    "revision": "b3e139d8d13397192c963dd7a32df967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e3ff328b79b5bd841c",
+    "url": "/static/css/2.5f966f18.chunk.css"
+  },
+  {
+    "revision": "31174416e907dad5f29e",
+    "url": "/static/css/3.d21749cd.chunk.css"
+  },
+  {
+    "revision": "cf20692c1739dbbb9b6e",
+    "url": "/static/css/main.740a324f.chunk.css"
+  },
+  {
+    "revision": "79e3ff328b79b5bd841c",
+    "url": "/static/js/2.a58ef76b.chunk.js"
+  },
+  {
+    "revision": "31174416e907dad5f29e",
+    "url": "/static/js/3.17cc14e8.chunk.js"
+  },
+  {
+    "revision": "9e95eb0ac00c4f89d7ec",
+    "url": "/static/js/4.98171411.chunk.js"
+  },
+  {
+    "revision": "cf20692c1739dbbb9b6e",
+    "url": "/static/js/main.0efb7537.chunk.js"
+  },
+  {
+    "revision": "b6243fcdef10923a2f7f",
+    "url": "/static/js/runtime~main.f49ba142.js"
+  },
+  {
+    "revision": "d20d098dccc3deb830808f97fb14f3ca",
+    "url": "/static/media/Diners-Club.d20d098d.jpg"
+  },
+  {
+    "revision": "92e2d23a1a2929823738532362c05eea",
+    "url": "/static/media/FlowerDimon.92e2d23a.jpg"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "/static/media/Roboto-BlackItalic.99682a78.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/static/media/Roboto-Italic.a720f17a.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/static/media/Roboto-MediumItalic.cf23e1bb.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "35a9c89aff1396595ad345e378a32aca",
+    "url": "/static/media/Roboto-ThinItalic.35a9c89a.ttf"
+  },
+  {
+    "revision": "c3f7641ef11bd405cfe12138f25f089f",
+    "url": "/static/media/cartoonFooter.c3f7641e.png"
+  },
+  {
+    "revision": "1d4ab251e668bd11b62b30615bd88e78",
+    "url": "/static/media/closeheart.1d4ab251.jpg"
+  },
+  {
+    "revision": "965dabcd31d04c27b319f732b7aae019",
+    "url": "/static/media/nosepin.965dabcd.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0a848483a9d0cc238c3b584e1260e3ec",
+    "url": "/static/media/visa.0a848483.jpg"
+  }
+]);
