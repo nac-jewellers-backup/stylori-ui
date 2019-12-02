b990d19b4361354f0d9a9b45026518a239cf3548
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2019-12-02 17:08:44 +0530

    Cache --- REACT EJECT

---
+++ build/precache-manifest.01b9dbcffe11e6b83f8c9f22cb99bc86.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b7f405f262dc66f3f4",
+    "url": "/0.css"
+  },
+  {
+    "revision": "03436afcc0e5a8f03bf3",
+    "url": "/2.css"
+  },
+  {
+    "revision": "ef7479f0079aad07cf6d",
+    "url": "/3.css"
+  },
+  {
+    "revision": "6a86a7f7657d47a558e40774e1280bc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03436afcc0e5a8f03bf3",
+    "url": "/static/css/2.5f966f18.chunk.css"
+  },
+  {
+    "revision": "ef7479f0079aad07cf6d",
+    "url": "/static/css/3.8d84f0c7.chunk.css"
+  },
+  {
+    "revision": "a3b7f405f262dc66f3f4",
+    "url": "/static/css/main.a2772578.chunk.css"
+  },
+  {
+    "revision": "03436afcc0e5a8f03bf3",
+    "url": "/static/js/2.bb05d3eb.chunk.js"
+  },
+  {
+    "revision": "ef7479f0079aad07cf6d",
+    "url": "/static/js/3.fe2aec7d.chunk.js"
+  },
+  {
+    "revision": "c715d764939ab523f57d",
+    "url": "/static/js/4.4b0381b4.chunk.js"
+  },
+  {
+    "revision": "a3b7f405f262dc66f3f4",
+    "url": "/static/js/main.2bc40267.chunk.js"
+  },
+  {
+    "revision": "91ef51be906eec02dab7",
+    "url": "/static/js/runtime~main.16e5146c.js"
+  },
+  {
+    "revision": "d20d098dccc3deb830808f97fb14f3ca",
+    "url": "/static/media/Diners-Club.d20d098d.jpg"
+  },
+  {
+    "revision": "92e2d23a1a2929823738532362c05eea",
+    "url": "/static/media/FlowerDimon.92e2d23a.jpg"
+  },
+  {
+    "revision": "c3f7641ef11bd405cfe12138f25f089f",
+    "url": "/static/media/cartoonFooter.c3f7641e.png"
+  },
+  {
+    "revision": "7996bd6992d473ba217275885f52812f",
+    "url": "/static/media/close.7996bd69.svg"
+  },
+  {
+    "revision": "1d4ab251e668bd11b62b30615bd88e78",
+    "url": "/static/media/closeheart.1d4ab251.jpg"
+  },
+  {
+    "revision": "965dabcd31d04c27b319f732b7aae019",
+    "url": "/static/media/nosepin.965dabcd.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0a848483a9d0cc238c3b584e1260e3ec",
+    "url": "/static/media/visa.0a848483.jpg"
+  }
+]);
