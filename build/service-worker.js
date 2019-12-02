2527f319db624d12bdcddac0ac15e57cb65b6d97
akaDPR <prakash@crayond.co>
2019-12-02 13:21:50 +0530

    google font"

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83ed2ce56b65743ee4f4898e46833d40.js"
+  "/precache-manifest.4715c0e599c5b662d3a24d00fac6cb61.js"
 );
 
 self.addEventListener('message', (event) => {
