0579459e22c9a9e6a8ee1f1c0cc03e3ea31c2753
akaDPR <prakash@crayond.co>
2019-12-02 16:11:05 +0530

    roboto font and logout process

--- src/components/Buynow/buynowbutton.js
+++ src/components/Buynow/buynowbutton.js
@@ -8,7 +8,7 @@ class Buynowbutton extends React.Component {
     render() {
         return (
             <div>
-                <Button className={this.props.class} style={{borderRadius:"5px",boxShadow:"rgba(216, 59, 59, 0.32) 0px 8px 15px"}}>
+                <Button className={this.props.class} style={{borderRadius:"5px"}}>
                     <i class="fa fa-shopping-bag buynow-icon"></i> <span className={this.props.button}>Buy Now</span>
              </Button>
             </div>
