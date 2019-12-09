7a8a4630d81a6169ec144b23c438085df5219ef1
akaDPR <prakash@crayond.co>
2019-12-09 17:53:52 +0530

    all page ui and bug fixes

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -144,7 +144,7 @@ class Header extends Component {
                      <img className="icons-header-sizes" src={usershape} />
         </Button> */}
 
-                    {localStorage.getItem("true") ?  
+                    {localStorage.getItem("true") ?
                       <span
                         aria-owns={open1}
                         aria-haspopup="true"
@@ -173,6 +173,7 @@ class Header extends Component {
                           localStorage.removeItem("email")
                           localStorage.removeItem("vals")
                           localStorage.removeItem("valuessetdata")
+                          localStorage.removeItem("true")
                           localStorage.removeItem("panel")
                           localStorage.removeItem("cartDetails")
                           window.location.reload()
