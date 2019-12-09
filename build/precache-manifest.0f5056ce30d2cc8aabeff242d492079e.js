13abd5eea9f8ca66e878c90fddb0fb9b4ba5bb8d
akaDPR <prakash@crayond.co>
2019-12-09 12:34:36 +0530

    all page ui and bug fixes

--- build/precache-manifest.0f5056ce30d2cc8aabeff242d492079e.js
+++ build/precache-manifest.0f5056ce30d2cc8aabeff242d492079e.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "19021c5805a605e385ec",
+    "revision": "88954844b27b81a8996c",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "19021c5805a605e385ec",
+    "revision": "88954844b27b81a8996c",
     "url": "/static/js/3.js"
   },
   {
