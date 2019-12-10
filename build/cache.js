e7e1cc20e40f5910fb57b1af88502aa9af15a6e2
Garuda_1 <arunkumaranke@gmail.com>
2019-12-10 15:45:19 +0530

    updated build

---
+++ build/cache.js
@@ -0,0 +1,48 @@
+
+
+const cacheFiles = [
+'/appversion.js',
+'/asset-manifest.json',
+'/cache.js',
+'/index.html',
+'/manifest.json'
+], _cacheName_one = 'test-cache';
+
+// NOTE: Caching custom files
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', function(event) {
+   
+    console.info('OFFLINE ENTRY TESTING','offline cache testing');
+    event.waitUntil(
+      caches.open(_cacheName_one).then(function(cache) {
+        return cache.addAll(cacheFiles );
+      })
+    );
+  });
+
+
+  self.addEventListener("fetch", e => {
+    console.info('FOUND-e', e);
+    e.respondWith(caches.match(e.request).then(async res => {
+        if (res) {
+            console.info('FOUND', e.request.url);
+            const cacheKeys = await caches.keys();
+            const ca = await caches.open(cacheKeys[0]);
+            const cb = await caches.open(cacheKeys[1]);
+            const inA = Boolean((await ca.keys()).find(r => r.url === e.request.url))
+            const inB = Boolean((await cb.keys()).find(r => r.url === e.request.url))
+            
+            console.info('LOADING CACHES FROM',inA ? cacheKeys[0] : cacheKeys[1]);
+            return caches.open(inA ? cacheKeys[0] : cacheKeys[1]).then(c => {
+                c.put(e.request.url, res.clone());
+                return res;
+            })
+        } else {
+            return fetch(e.request);
+        }
+    }).catch(err => {
+        if (err) {
+            console.info('ERROR CACHING')
+        }
+    }))
+})
