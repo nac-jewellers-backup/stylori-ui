606f34326f62474fbd54c0a6daafd03b73bd2a0c
sandeep-at-595182351008 <prakash@crayond.co>
2019-12-18 17:59:05 +0530

    remove wishlist

---
+++ build/precache-manifest.3c84e459a8db82f0e0f7630d32f540fb.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f8659f4184073d1a743d9b2dc2e621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e81745f683a1adcda9",
+    "url": "/static/css/2.css"
+  },
+  {
+    "revision": "bf1cf5902130c742e9f9",
+    "url": "/static/css/3.css"
+  },
+  {
+    "revision": "9573268475f0b577cf41",
+    "url": "/static/css/4.css"
+  },
+  {
+    "revision": "413b71c9a2280b068da2",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "a3e81745f683a1adcda9",
+    "url": "/static/js/2.js"
+  },
+  {
+    "revision": "bf1cf5902130c742e9f9",
+    "url": "/static/js/3.js"
+  },
+  {
+    "revision": "9573268475f0b577cf41",
+    "url": "/static/js/4.js"
+  },
+  {
+    "revision": "413b71c9a2280b068da2",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "265884b522a19a75c49d",
+    "url": "/static/js/runtime~main.js"
+  },
+  {
+    "revision": "d20d098dccc3deb830808f97fb14f3ca",
+    "url": "/static/media/Diners-Club.jpg"
+  },
+  {
+    "revision": "92e2d23a1a2929823738532362c05eea",
+    "url": "/static/media/FlowerDimon.jpg"
+  },
+  {
+    "revision": "c3f7641ef11bd405cfe12138f25f089f",
+    "url": "/static/media/cartoonFooter.png"
+  },
+  {
+    "revision": "2b3d63f761a61e5c65a4773359c4eb40",
+    "url": "/static/media/close.svg"
+  },
+  {
+    "revision": "1d4ab251e668bd11b62b30615bd88e78",
+    "url": "/static/media/closeheart.jpg"
+  },
+  {
+    "revision": "731e5ecd2c7827cef3f847ec91080214",
+    "url": "/static/media/delivery.svg"
+  },
+  {
+    "revision": "28cd30ea4ad92b4520d9a3fb2634a87d",
+    "url": "/static/media/img1ProductModal.jpg"
+  },
+  {
+    "revision": "b944ac0307e92dae56a16dc18b6318a1",
+    "url": "/static/media/img2ProductModal.jpg"
+  },
+  {
+    "revision": "2e0c3b28d442f07cf1fb810420137dc5",
+    "url": "/static/media/img3ProductModal.jpg"
+  },
+  {
+    "revision": "337eef9d867ffd752a548702f05cb8e6",
+    "url": "/static/media/img4ProductModal.jpg"
+  },
+  {
+    "revision": "6e25bad6f003be8fe1ddf1c68d2b4dd9",
+    "url": "/static/media/logout.svg"
+  },
+  {
+    "revision": "162c55add6a47b0d4045ee2e69d2e824",
+    "url": "/static/media/love.svg"
+  },
+  {
+    "revision": "965dabcd31d04c27b319f732b7aae019",
+    "url": "/static/media/nosepin.gif"
+  },
+  {
+    "revision": "57b56bb10faf3b9dbb17bc91e6be7baf",
+    "url": "/static/media/shopping.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.woff"
+  },
+  {
+    "revision": "4ab5e81bb7847802b7a01ffe64e69740",
+    "url": "/static/media/stylori-silver-logo-small-screen.png"
+  },
+  {
+    "revision": "92c0fa80ff8c401b154438a2c4ba0b5a",
+    "url": "/static/media/stylori-silver-logo.png"
+  },
+  {
+    "revision": "26f4adaec0fa060fe6ef92c4c4f7c485",
+    "url": "/static/media/telephone.svg"
+  },
+  {
+    "revision": "1737e5e0a88de55dc6cf1a773e4b0816",
+    "url": "/static/media/topPicksOne.jpg"
+  },
+  {
+    "revision": "35f5ee82b7e9b5e66f70a99aefcb3865",
+    "url": "/static/media/topPicksThree.jpg"
+  },
+  {
+    "revision": "37b89e280ecc047a56d66fdb6404bb23",
+    "url": "/static/media/topPicksTwo.jpg"
+  },
+  {
+    "revision": "bcc776345852aa9836e64bcc3e000864",
+    "url": "/static/media/user-shape.svg"
+  },
+  {
+    "revision": "0a848483a9d0cc238c3b584e1260e3ec",
+    "url": "/static/media/visa.jpg"
+  }
+]);
