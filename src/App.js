ca078eb17f2cb6ccc9c604785c7aff72b85a4b72
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2019-12-23 20:48:35 +0530

    infinity scroll

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './index.css'
 import Loading from 'screens/Loading';
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import  Theme  from './Theme.js'
+import { lambda_func_front_end } from './utils'
+import Theme from './Theme.js'
 import { withRouter } from 'react-router-dom';
 let jewellery_theme = createMuiTheme(require('./jewellery_theme.json'));
 let silver_jewellery_theme = createMuiTheme(require('./silver_jewellery_theme.json'));
@@ -14,44 +15,49 @@ let silverThemes = responsiveFontSizes(silver_jewellery_theme);
 const RouterApp = React.lazy(() => import('router'));
 
 
+
 // const client = new ApolloClient({
 //   uri: ``,
 // });
 
-const theme_func = (props) =>{
-  console.log('props.match.path', props.match.path)
+const theme_func = () => {
 
-if(props.location.pathname === "/silverjewellery" || props.location.pathname === "/silver-jewellery"){
-  return silverThemes
+  if (window.location.pathname === "/silverjewellery") {
+    return silverThemes
+  }
+  else {
+    return jewelleryThemes
+  }
+
 }
-else{
-  return jewelleryThemes
-}
 
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  async componentDidMount(){
+    lambda_func_front_end()
+  }
+  render() {
+    return (
+      // <ApolloProvider client={client}>
+      <GlobalProvider>
+        <Theme>
+          <NetworkProvider>
+            {/*productId="SP1135" <ProductDetailProvider productId="SP1135">*/}
+            <Router>
+              <React.Suspense fallback={Loading} >
+                <RouterApp />
+              </React.Suspense>
+            </Router>
+            {/* </ProductDetailProvider> */}
+          </NetworkProvider>
+        </Theme>
+      </GlobalProvider>
+      // </ApolloProvider>
+    );
+  }
 
-function App(props) {
-
-
-  // const productId = props.location.state.data
-
-  return (
-    // <ApolloProvider client={client}>
-    <GlobalProvider>
-      <Theme>
-        <NetworkProvider>
-          {/*productId="SP1135" <ProductDetailProvider productId="SP1135">*/}
-          <Router>
-            <React.Suspense fallback={Loading} >
-              <RouterApp />
-            </React.Suspense>
-          </Router>
-          {/* </ProductDetailProvider> */}
-        </NetworkProvider>
-      </Theme>
-    </GlobalProvider>
-    // </ApolloProvider>
-  );
 }
 
 export default withRouter(App);
