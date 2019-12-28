75df5501def401e20ab9b4065a7afe80f0627912
unknown <prakash>
2019-12-29 01:17:03 +0530

    merg config

--- build/precache-manifest.9b82a827070b35b4c2f4ef566387fd7e.js
+++ build/precache-manifest.9b82a827070b35b4c2f4ef566387fd7e.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "5c0dccbfcb115250b738",
+    "revision": "58b411d0493f2f29bd90",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "5c0dccbfcb115250b738",
+    "revision": "58b411d0493f2f29bd90",
     "url": "/static/js/3.js"
   },
   {
