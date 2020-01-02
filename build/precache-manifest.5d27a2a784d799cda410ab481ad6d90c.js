7ffc012053255c3ac30626f5ac4a7e1949167a99
unknown <prakash>
2020-01-03 00:58:07 +0530

    checkout bug fies

--- build/precache-manifest.5d27a2a784d799cda410ab481ad6d90c.js
+++ build/precache-manifest.5d27a2a784d799cda410ab481ad6d90c.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "fe52448f0f9833fbc8cd",
+    "revision": "e43e4e68800ad29069ee",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "fe52448f0f9833fbc8cd",
+    "revision": "e43e4e68800ad29069ee",
     "url": "/static/js/3.js"
   },
   {
