99f3f5fd4134792672f730380337bc0b75839fce
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-01-03 09:26:58 +0530

    Build file created latest of Arun, Prakash, Selvan and Jayasoorya

--- build/precache-manifest.3a6e3faed526ab8d8cd7c911cc86b357.js
+++ build/precache-manifest.3a6e3faed526ab8d8cd7c911cc86b357.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "4d015bfb316a0805a9fe",
+    "revision": "d20e5b488b3eb8ac0e3c",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "4d015bfb316a0805a9fe",
+    "revision": "d20e5b488b3eb8ac0e3c",
     "url": "/static/js/3.js"
   },
   {
