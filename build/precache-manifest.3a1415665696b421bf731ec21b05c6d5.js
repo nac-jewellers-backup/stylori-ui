bdf5cde7af659264510c06264f386cea8deeb698
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-01-07 14:26:42 +0530

    Build file created

--- build/precache-manifest.3a1415665696b421bf731ec21b05c6d5.js
+++ build/precache-manifest.3a1415665696b421bf731ec21b05c6d5.js
@@ -8,11 +8,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "d41287478dc263d5d900",
+    "revision": "13f5c75553bdc936d197",
     "url": "/static/css/3.css"
   },
   {
-    "revision": "89d45b8c94bf2c0210e5",
+    "revision": "71e8f27e4824cda9fa41",
     "url": "/static/css/main.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "d41287478dc263d5d900",
+    "revision": "13f5c75553bdc936d197",
     "url": "/static/js/3.js"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "89d45b8c94bf2c0210e5",
+    "revision": "71e8f27e4824cda9fa41",
     "url": "/static/js/main.js"
   },
   {
