644bc1e5940224a97d5b29f92629ea0026284a59
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-01-08 12:53:14 +0530

    Updated the issues -----CONTAINER fixes in checkout issue

--- build/precache-manifest.a1451a404cf6a52f2b36ddb1e192fdb1.js
+++ build/precache-manifest.a1451a404cf6a52f2b36ddb1e192fdb1.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "1c8104f56416b6b8d2ad",
+    "revision": "08943936d673522af240",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "1c8104f56416b6b8d2ad",
+    "revision": "08943936d673522af240",
     "url": "/static/js/3.js"
   },
   {
