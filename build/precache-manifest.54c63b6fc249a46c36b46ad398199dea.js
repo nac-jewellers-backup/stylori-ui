97cb35fcc133a8951d04afe310581feb0c54ab12
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-01-08 13:17:21 +0530

    latest build with stabilized as on ---  7/01/2020

--- build/precache-manifest.54c63b6fc249a46c36b46ad398199dea.js
+++ build/precache-manifest.54c63b6fc249a46c36b46ad398199dea.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "08943936d673522af240",
+    "revision": "c520210a963ef60022b3",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "08943936d673522af240",
+    "revision": "c520210a963ef60022b3",
     "url": "/static/js/3.js"
   },
   {
