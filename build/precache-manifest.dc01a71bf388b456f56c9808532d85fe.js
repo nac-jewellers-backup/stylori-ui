4659fc88aba6eb370afc83da7bf048567e7cb1e6
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-01-08 14:42:50 +0530

    latest push with payment route -----------  stabilized version

--- build/precache-manifest.dc01a71bf388b456f56c9808532d85fe.js
+++ build/precache-manifest.dc01a71bf388b456f56c9808532d85fe.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "7b8624da4966b493769c",
+    "revision": "cb2369621d1502f36d74",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "7b8624da4966b493769c",
+    "revision": "cb2369621d1502f36d74",
     "url": "/static/js/3.js"
   },
   {
