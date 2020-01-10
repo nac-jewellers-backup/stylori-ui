d41a9405759da4bced7afc3c18406b8bf3c32d3e
selvanradha <selvan@crayond.co>
2020-01-10 15:58:18 +0530

    upadated build

--- build/precache-manifest.04f25801fa9d9d806da27f7bee0c28a0.js
+++ build/precache-manifest.04f25801fa9d9d806da27f7bee0c28a0.js
@@ -8,11 +8,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "514ec307a97dc589ee3e",
+    "revision": "4d4ddb36e622eef43053",
     "url": "/static/css/3.css"
   },
   {
-    "revision": "d03f612b24277c45c075",
+    "revision": "b7feffa16a2a56fe26f6",
     "url": "/static/css/main.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "514ec307a97dc589ee3e",
+    "revision": "4d4ddb36e622eef43053",
     "url": "/static/js/3.js"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "d03f612b24277c45c075",
+    "revision": "b7feffa16a2a56fe26f6",
     "url": "/static/js/main.js"
   },
   {
