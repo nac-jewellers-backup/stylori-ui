77f6844f0a057727d145ce56ac9e9bc0901952f9
selvanradha <selvan@crayond.co>
2020-01-21 17:43:36 +0530

    updated build

--- build/precache-manifest.600cd47fa85c0c256a444b3a672df9e9.js
+++ build/precache-manifest.600cd47fa85c0c256a444b3a672df9e9.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "48086913576cc22df3cf",
+    "revision": "07d060e4d6aa7669a853",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "48086913576cc22df3cf",
+    "revision": "07d060e4d6aa7669a853",
     "url": "/static/js/3.js"
   },
   {
