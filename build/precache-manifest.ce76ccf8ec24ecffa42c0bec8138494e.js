d1a18b42ce9acf018cfcd5bfde70c25b98091833
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-01-24 17:24:46 +0530

    updated the payment hidden form issue fixed

--- build/precache-manifest.ce76ccf8ec24ecffa42c0bec8138494e.js
+++ build/precache-manifest.ce76ccf8ec24ecffa42c0bec8138494e.js
@@ -8,11 +8,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "d0e9380d0827a787610c",
+    "revision": "3e5071373732dd98ba95",
     "url": "/static/css/3.css"
   },
   {
-    "revision": "9786bceb47314f457010",
+    "revision": "4981c92f6dc8b207b527",
     "url": "/static/css/main.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "d0e9380d0827a787610c",
+    "revision": "3e5071373732dd98ba95",
     "url": "/static/js/3.js"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "9786bceb47314f457010",
+    "revision": "4981c92f6dc8b207b527",
     "url": "/static/js/main.js"
   },
   {
@@ -48,11 +48,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/Roboto-Regular.ttf"
   },
   {
-    "revision": "5c8caf8f7ea7126619cea19d463c5146",
+    "revision": "fadbed18b9fafe0e25c8c0fb8894189f",
     "url": "/static/media/Stylori Silver logo.svg"
   },
   {
-    "revision": "04639ae8969c77110a53303b92b57712",
+    "revision": "271780ce712885f13b93200ae25f1288",
     "url": "/static/media/Stylorilogo.svg"
   },
   {
@@ -80,7 +80,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/img4ProductModal.jpg"
   },
   {
-    "revision": "51fc5370cb4d7e537c06ea117f090ba2",
+    "revision": "6e25bad6f003be8fe1ddf1c68d2b4dd9",
     "url": "/static/media/logout.svg"
   },
   {
