cbfeb309f36788436ce6b145645b1c3642ca1abd
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-01-25 16:10:52 +0530

    Updated the wishlist bug add and delete

--- build/precache-manifest.7a6195ac17e05e9a27dbb4cc0a07f4dc.js
+++ build/precache-manifest.7a6195ac17e05e9a27dbb4cc0a07f4dc.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "6a11bd68ee22f4cb7e60",
+    "revision": "c40f67b7bd2ebfbeab17",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "6a11bd68ee22f4cb7e60",
+    "revision": "c40f67b7bd2ebfbeab17",
     "url": "/static/js/3.js"
   },
   {
