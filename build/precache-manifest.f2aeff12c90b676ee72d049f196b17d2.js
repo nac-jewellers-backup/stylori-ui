bfbad64af260bdb368b4b2fd2f11028ab3125a4e
prakash <prakash@crayond.co>
2020-01-28 14:54:38 +0530

    filter listing page chips

--- build/precache-manifest.f2aeff12c90b676ee72d049f196b17d2.js
+++ build/precache-manifest.f2aeff12c90b676ee72d049f196b17d2.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "d5a567a5139772e0e1e4",
+    "revision": "b8c7325d600082fa95d6",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "d5a567a5139772e0e1e4",
+    "revision": "b8c7325d600082fa95d6",
     "url": "/static/js/3.js"
   },
   {
