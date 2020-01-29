ccf4c43df162336fd26705f65e8126cc15eca32a
prakash <prakash@crayond.co>
2020-01-29 20:48:13 +0530

    content changes

--- build/precache-manifest.2fb37ed76d048f09aa6235800c83420e.js
+++ build/precache-manifest.2fb37ed76d048f09aa6235800c83420e.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "08459bfac968906e093c",
+    "revision": "a3da962acbfac14beb64",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "08459bfac968906e093c",
+    "revision": "a3da962acbfac14beb64",
     "url": "/static/js/3.js"
   },
   {
