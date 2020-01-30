970045888632e255b9e11c9a756aa2aada78dd25
prakash <prakash@crayond.co>
2020-01-30 20:11:36 +0530

    buynow button ui fixes

--- build/precache-manifest.c459ff722c79bcb40ddefbdba2047bce.js
+++ build/precache-manifest.c459ff722c79bcb40ddefbdba2047bce.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "ba1118b7a08dc0fc74c6",
+    "revision": "550ece645e7aa677377f",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "ba1118b7a08dc0fc74c6",
+    "revision": "550ece645e7aa677377f",
     "url": "/static/js/3.js"
   },
   {
