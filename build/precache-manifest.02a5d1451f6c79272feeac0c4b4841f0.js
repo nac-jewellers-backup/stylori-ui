5910e554f7557c0fa1661199e796008173cddcf0
prakash <prakash@crayond.co>
2020-01-30 15:41:47 +0530

    cart num of counts in header bug fixes solved

--- build/precache-manifest.02a5d1451f6c79272feeac0c4b4841f0.js
+++ build/precache-manifest.02a5d1451f6c79272feeac0c4b4841f0.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "52bff40d8c6eab89b60b",
+    "revision": "356b3e724b473ffe51ee",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "52bff40d8c6eab89b60b",
+    "revision": "356b3e724b473ffe51ee",
     "url": "/static/js/3.js"
   },
   {
