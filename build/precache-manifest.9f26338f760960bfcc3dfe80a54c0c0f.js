0a968486f0f8871461fd4847c05cce6f95df7149
selvanradha <selvan@crayond.co>
2020-01-30 19:20:51 +0530

    updated cart

--- build/precache-manifest.9f26338f760960bfcc3dfe80a54c0c0f.js
+++ build/precache-manifest.9f26338f760960bfcc3dfe80a54c0c0f.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.css"
   },
   {
-    "revision": "1c144a0343161dce4088",
+    "revision": "28cac8bbb8ca6204c038",
     "url": "/static/css/main.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "1c144a0343161dce4088",
+    "revision": "28cac8bbb8ca6204c038",
     "url": "/static/js/main.js"
   },
   {
