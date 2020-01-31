866ce60163cebebdba97851f9088269419da884f
prakash <prakash@crayond.co>
2020-01-31 11:21:44 +0530

    logout fixes in responseive

--- build/precache-manifest.108ac29c87f4cf74b984a762617ad95e.js
+++ build/precache-manifest.108ac29c87f4cf74b984a762617ad95e.js
@@ -4,31 +4,31 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/index.html"
   },
   {
-    "revision": "bb4df13ab00969d18bb3",
+    "revision": "7ad8d8643dd29ad14c4b",
     "url": "/static/css/2.css"
   },
   {
-    "revision": "717a15462346fa1ba3f1",
+    "revision": "22d674a9c4ff2c336a2d",
     "url": "/static/css/3.css"
   },
   {
-    "revision": "493fa5eeff088015e531",
+    "revision": "8af8d7cbe8f23c719d97",
     "url": "/static/css/main.css"
   },
   {
-    "revision": "bb4df13ab00969d18bb3",
+    "revision": "7ad8d8643dd29ad14c4b",
     "url": "/static/js/2.js"
   },
   {
-    "revision": "717a15462346fa1ba3f1",
+    "revision": "22d674a9c4ff2c336a2d",
     "url": "/static/js/3.js"
   },
   {
-    "revision": "273422b04b5db358a519",
+    "revision": "b6741f262c1b9456d463",
     "url": "/static/js/4.js"
   },
   {
-    "revision": "493fa5eeff088015e531",
+    "revision": "8af8d7cbe8f23c719d97",
     "url": "/static/js/main.js"
   },
   {
