912eafaedec6329c99228c3700febef22930a2b5
prakash <prakash@crayond.co>
2020-02-04 18:02:24 +0530

    my acc persnol information details issues solved

---
+++ build/precache-manifest.389497e239af4db948bdb0319dc03ac1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369c02b16313098db522644787bc26a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac8f682a06f09446c21",
+    "url": "/static/css/2.css"
+  },
+  {
+    "revision": "582298342ddccfb9fa15",
+    "url": "/static/css/3.css"
+  },
+  {
+    "revision": "f8a23009d896ea671977",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "1ac8f682a06f09446c21",
+    "url": "/static/js/2.js"
+  },
+  {
+    "revision": "582298342ddccfb9fa15",
+    "url": "/static/js/3.js"
+  },
+  {
+    "revision": "ba8e55369c6b29ffed09",
+    "url": "/static/js/4.js"
+  },
+  {
+    "revision": "f8a23009d896ea671977",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "67501ddb9e15004a2ab4",
+    "url": "/static/js/runtime~main.js"
+  },
+  {
+    "revision": "d20d098dccc3deb830808f97fb14f3ca",
+    "url": "/static/media/Diners-Club.jpg"
+  },
+  {
+    "revision": "92e2d23a1a2929823738532362c05eea",
+    "url": "/static/media/FlowerDimon.jpg"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "5c8caf8f7ea7126619cea19d463c5146",
+    "url": "/static/media/Stylori Silver logo.svg"
+  },
+  {
+    "revision": "04639ae8969c77110a53303b92b57712",
+    "url": "/static/media/Stylorilogo.svg"
+  },
+  {
+    "revision": "c3f7641ef11bd405cfe12138f25f089f",
+    "url": "/static/media/cartoonFooter.png"
+  },
+  {
+    "revision": "1d4ab251e668bd11b62b30615bd88e78",
+    "url": "/static/media/closeheart.jpg"
+  },
+  {
+    "revision": "28cd30ea4ad92b4520d9a3fb2634a87d",
+    "url": "/static/media/img1ProductModal.jpg"
+  },
+  {
+    "revision": "b944ac0307e92dae56a16dc18b6318a1",
+    "url": "/static/media/img2ProductModal.jpg"
+  },
+  {
+    "revision": "2e0c3b28d442f07cf1fb810420137dc5",
+    "url": "/static/media/img3ProductModal.jpg"
+  },
+  {
+    "revision": "337eef9d867ffd752a548702f05cb8e6",
+    "url": "/static/media/img4ProductModal.jpg"
+  },
+  {
+    "revision": "51fc5370cb4d7e537c06ea117f090ba2",
+    "url": "/static/media/logout.svg"
+  },
+  {
+    "revision": "965dabcd31d04c27b319f732b7aae019",
+    "url": "/static/media/nosepin.gif"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.woff"
+  },
+  {
+    "revision": "681db1b372ef45af725df7762fcb49ee",
+    "url": "/static/media/styloriGagets.png"
+  },
+  {
+    "revision": "1737e5e0a88de55dc6cf1a773e4b0816",
+    "url": "/static/media/topPicksOne.jpg"
+  },
+  {
+    "revision": "35f5ee82b7e9b5e66f70a99aefcb3865",
+    "url": "/static/media/topPicksThree.jpg"
+  },
+  {
+    "revision": "37b89e280ecc047a56d66fdb6404bb23",
+    "url": "/static/media/topPicksTwo.jpg"
+  },
+  {
+    "revision": "0a848483a9d0cc238c3b584e1260e3ec",
+    "url": "/static/media/visa.jpg"
+  }
+]);
