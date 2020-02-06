864483a345c25082eafc8f9874eca3c1d2abeb3c
prakash <prakash@crayond.co>
2020-02-06 20:22:11 +0530

    addres issues

--- build/precache-manifest.eb1fe3762893785b7e6d52658577b18b.js
+++ build/precache-manifest.eb1fe3762893785b7e6d52658577b18b.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "c1e1c7bf09d1e74537a1",
+    "revision": "aeb2b59874e2c80ffeb6",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "c1e1c7bf09d1e74537a1",
+    "revision": "aeb2b59874e2c80ffeb6",
     "url": "/static/js/3.js"
   },
   {
