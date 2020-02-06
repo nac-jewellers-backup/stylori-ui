c424966aa67f4973c2f6971ceb0b54c04eb5622c
vimal-crayond <vimal@crayond.co>
2020-02-06 20:04:44 +0530

    number

--- src/components/product-image-slider/buyNow.js
+++ src/components/product-image-slider/buyNow.js
@@ -49,6 +49,7 @@ const inputsearch = (props, state, handleChanges, handleCodChange) => {
                             onKeyPress={(e) => { if (!(e.which >= 48 && e.which <= 57)) e.preventDefault(); }}
                         /> */}
                         <input onkeyup="this.value=this.value.replace(/[^0-9]/g,'');"
+                            type="number"
                             placeholder="Enter Pincode"
                             maxLength={6}
                             className="pincode-cust buynow-search"
@@ -113,7 +114,7 @@ const Buydetails = (props, state, handleChanges, handleCodChange, canceldeletech
                                     <Typography>
                                         <i class="fa fa-whatsapp overall-icons" aria-hidden="true"></i>&nbsp;
                                 </Typography>
-                                    <Typography className={classes.TypoListed}>
+                                    <Typography  className={classes.TypoListed}>
                                         {val.phonenum}
                                     </Typography>
                                 </Grid>
