a912c3e1d436bbf0f758f1634d81bb1deefb17c0
prakash <prakash@crayond.co>
2020-02-07 13:31:45 +0530

    checkout addres issues solves

--- build/precache-manifest.2182c9787555256c84163874587e74c6.js
+++ build/precache-manifest.2182c9787555256c84163874587e74c6.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "099cba395e5accd4f6fe",
+    "revision": "141fb95b5e12af0328bd",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "099cba395e5accd4f6fe",
+    "revision": "141fb95b5e12af0328bd",
     "url": "/static/js/3.js"
   },
   {
