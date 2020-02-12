2f9f1f8d9f598ca3d548ff0577ad17fee0f7bc06
JAYASOORYA <JAYASOORYA@CRAYOND.CO>
2020-02-12 16:43:19 +0530

    new details in cart have been updated

--- src/queries/cart.js
+++ src/queries/cart.js
@@ -1,34 +1,48 @@
 export const CART = `query myquerycart($productList: [String!]) {
   allTransSkuLists(filter: {generatedSku: {in: $productList}}) {
     nodes {
-      generatedSku 
-      purity 
+      generatedSku
+      purity
       metalColor
       discountPrice
       markupPrice
       skuUrl
-      
       productListByProductId {
         productId
         productName
-        productImagesByProductId (filter: { imagePosition: {equalTo: 1}}){
+        productImagesByProductId(filter: {imagePosition: {equalTo: 1}}) {
           nodes {
             imageUrl
           }
         }
-        productType 
+        productType
         productMaterialsByProductSku {
           nodes {
             materialName
           }
         }
-       }
+        productDiamondsByProductSku {
+          nodes {
+            diamondColour
+            diamondShape
+            stoneWeight
+          }
+        }
+        productGemstonesByProductSku {
+          nodes {
+            gemstoneSize
+            gemstoneType
+            stoneWeight
+          }
+        }
+      }
       skuWeight
       diamondType
       skuSize
     }
   }
-}`
+}
+`
 export const ALLORDERS = `query MyQuery($userProfileId: [UUID!]) {
   allOrders(filter: {userProfileId: {in: $userProfileId}}, orderBy: CREATED_AT_DESC, condition: {paymentStatus: "Submitted"}) {
     nodes {
