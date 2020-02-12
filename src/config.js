ee90bbb231406c689071f6059b54256d5466236e
Your Name <jayasoorya@crayond.co>
2020-02-12 17:32:09 +0530

    updated the beta issue

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://api-staging.stylori.com" 
 export const HOME_PAGE_URL = ""
 export const CDN_URL = "https://assets.stylori.com/"
-// export const CDN_URL = "https://styloriimages.s3.ap-south-1.amazonaws.com/"
+// export const CDN_URL = "https://styloriimages.s3.ap-south-1.amazonaws.com/" 
