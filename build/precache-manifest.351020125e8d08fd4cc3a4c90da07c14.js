834c0aae0304879ebcac7fecf2faaa9019614824
Your Name <jayasoorya@crayond.co>
2020-02-12 20:23:03 +0530

    updated the beta final version with staging branch merge file

--- build/precache-manifest.351020125e8d08fd4cc3a4c90da07c14.js
+++ build/precache-manifest.351020125e8d08fd4cc3a4c90da07c14.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.css"
   },
   {
-    "revision": "d61548e111b60e4351a9",
+    "revision": "54eead35e711900e5751",
     "url": "/static/css/main.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "d61548e111b60e4351a9",
+    "revision": "54eead35e711900e5751",
     "url": "/static/js/main.js"
   },
   {
