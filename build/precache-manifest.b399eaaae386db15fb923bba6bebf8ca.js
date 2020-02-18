f790e9305a614e3e57137ea4e2bfe4c61bb4b7ab
Your Name <jayasoorya@crayond.co>
2020-02-18 14:06:31 +0530

    Updated the small screen carousel color

--- build/precache-manifest.b399eaaae386db15fb923bba6bebf8ca.js
+++ build/precache-manifest.b399eaaae386db15fb923bba6bebf8ca.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "b974111b0fd12ea8bbd3",
+    "revision": "7bf9c5c36b78498da60d",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "b974111b0fd12ea8bbd3",
+    "revision": "7bf9c5c36b78498da60d",
     "url": "/static/js/3.js"
   },
   {
