6c145d143e51365cbbdb37423cec793b6f6d325f
Your Name <jayasoorya@crayond.co>
2020-02-18 13:41:09 +0530

    Updated the elastic issue

--- build/precache-manifest.211c7cfb6c71b3254ae36cb39efc2a9d.js
+++ build/precache-manifest.211c7cfb6c71b3254ae36cb39efc2a9d.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "38ca7e543e85d8be0b01",
+    "revision": "b974111b0fd12ea8bbd3",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "38ca7e543e85d8be0b01",
+    "revision": "b974111b0fd12ea8bbd3",
     "url": "/static/js/3.js"
   },
   {
