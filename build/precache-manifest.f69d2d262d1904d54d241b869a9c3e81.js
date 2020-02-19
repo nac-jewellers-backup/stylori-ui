c072e4cfb5a2504aef0a8c65e3ef8de7c7c95f5b
Your Name <jayasoorya@crayond.co>
2020-02-19 17:59:03 +0530

    updated the meta tag issue

--- build/precache-manifest.f69d2d262d1904d54d241b869a9c3e81.js
+++ build/precache-manifest.f69d2d262d1904d54d241b869a9c3e81.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "04a2e4a06f9fdd0e6575",
+    "revision": "9eae060fe67ebb5344b5",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "04a2e4a06f9fdd0e6575",
+    "revision": "9eae060fe67ebb5344b5",
     "url": "/static/js/3.js"
   },
   {
