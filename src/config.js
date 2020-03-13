c38997eed054e1ef07995bb2e83463df2cb773a9
Your Name <jayasoorya@crayond.co>
2020-03-13 17:46:54 +0530

    staging url fixes for CDN

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ export const API_URL = "https://api-staging.stylori.com"
 
 export const HOME_PAGE_URL = ""
 // export const CDN_URL = "https://assets.stylori.com/"
-export const CDN_URL = "https://styloriimages-staging.s3.ap-south-1.amazonaws.com/"
+export const CDN_URL =  "https://styloriimages-staging.s3.ap-south-1.amazonaws.com/"
 // export const CDN_URL = "https://styloriimages.s3.ap-south-1.amazonaws.com/" 
