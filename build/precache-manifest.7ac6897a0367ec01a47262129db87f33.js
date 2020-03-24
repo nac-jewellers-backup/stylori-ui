e8bc34950b3d02aa6f98ab895c898253cecbd61f
Your Name <jayasoorya@crayond.co>
2020-03-24 12:00:34 +0530

    testing cache issue

--- build/precache-manifest.7ac6897a0367ec01a47262129db87f33.js
+++ build/precache-manifest.7ac6897a0367ec01a47262129db87f33.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "7867a4e8ed56227308ed",
+    "revision": "5a3768c78c7c7d4833f8",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "7867a4e8ed56227308ed",
+    "revision": "5a3768c78c7c7d4833f8",
     "url": "/static/js/3.js"
   },
   {
