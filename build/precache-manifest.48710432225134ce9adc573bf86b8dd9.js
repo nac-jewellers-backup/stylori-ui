f8f292323536b1eedda3f494db30b0cf393d354f
Your Name <jayasoorya@crayond.co>
2020-04-24 12:44:48 +0530

    Updated : disabled the cache issue

--- build/precache-manifest.48710432225134ce9adc573bf86b8dd9.js
+++ build/precache-manifest.48710432225134ce9adc573bf86b8dd9.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "cc0ec06942a204435d3b",
+    "revision": "bc4e7c7264b0a52c0d03",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "cc0ec06942a204435d3b",
+    "revision": "bc4e7c7264b0a52c0d03",
     "url": "/static/js/3.js"
   },
   {
