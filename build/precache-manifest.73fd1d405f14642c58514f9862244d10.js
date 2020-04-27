f0421e1e19b89c675f66b69781b3dde2e96e24d2
Your Name <jayasoorya@crayond.co>
2020-04-27 12:49:30 +0530

    updated : Redirecting for isActive false product

--- build/precache-manifest.73fd1d405f14642c58514f9862244d10.js
+++ build/precache-manifest.73fd1d405f14642c58514f9862244d10.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.css"
   },
   {
-    "revision": "68231b858d51992dc771",
+    "revision": "2aa320ec79db9d9008a8",
     "url": "/static/css/main.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "68231b858d51992dc771",
+    "revision": "2aa320ec79db9d9008a8",
     "url": "/static/js/main.js"
   },
   {
