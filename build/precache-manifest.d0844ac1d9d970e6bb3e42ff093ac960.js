90c1447a568207a836e475951f5281c20a3f0054
Your Name <jayasoorya@crayond.co>
2020-05-18 22:02:57 +0530

    service worker file changed

--- build/precache-manifest.d0844ac1d9d970e6bb3e42ff093ac960.js
+++ build/precache-manifest.d0844ac1d9d970e6bb3e42ff093ac960.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.css"
   },
   {
-    "revision": "4159dd8805b2b9282272",
+    "revision": "9d830a5dacf59db57ceb",
     "url": "/static/css/main.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "4159dd8805b2b9282272",
+    "revision": "9d830a5dacf59db57ceb",
     "url": "/static/js/main.js"
   },
   {
