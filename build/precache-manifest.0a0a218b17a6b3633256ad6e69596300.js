1bbf0f2e4cc0c7a317940882e045a9b624ae1bef
Your Name <jayasoorya@crayond.co>
2020-06-08 16:40:58 +0530

    Updated the phone number issue -- in checkout -- BUILD FILE

--- build/precache-manifest.0a0a218b17a6b3633256ad6e69596300.js
+++ build/precache-manifest.0a0a218b17a6b3633256ad6e69596300.js
@@ -8,11 +8,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "5172414577254b614ff4",
+    "revision": "feb75d63aa862c4fee1c",
     "url": "/static/css/3.css"
   },
   {
-    "revision": "bfe25e769a75146f2a02",
+    "revision": "7bfe0249b25385634897",
     "url": "/static/css/main.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "5172414577254b614ff4",
+    "revision": "feb75d63aa862c4fee1c",
     "url": "/static/js/3.js"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "bfe25e769a75146f2a02",
+    "revision": "7bfe0249b25385634897",
     "url": "/static/js/main.js"
   },
   {
