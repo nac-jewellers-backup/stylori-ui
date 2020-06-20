9c0eb2955f8c7669e37b29e1e836358888a4b9bb
Your Name <jayasoorya@crayond.co>
2020-06-20 17:28:43 +0530

    Updated the silver collections to image to imageurl

--- build/precache-manifest.2ca1aa4ad62ec5855be0babf45e53543.js
+++ build/precache-manifest.2ca1aa4ad62ec5855be0babf45e53543.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "163a3141fc7e7df7c2a8",
+    "revision": "8907e4241e87afe5a949",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "163a3141fc7e7df7c2a8",
+    "revision": "8907e4241e87afe5a949",
     "url": "/static/js/3.js"
   },
   {
