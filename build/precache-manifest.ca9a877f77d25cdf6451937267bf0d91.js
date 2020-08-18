01f4ce20f0c93940761f66e3615f6d7b8872a19a
Your Name <jayasoorya@crayond.co>
2020-08-18 15:09:20 +0530

    banner changes

--- build/precache-manifest.ca9a877f77d25cdf6451937267bf0d91.js
+++ build/precache-manifest.ca9a877f77d25cdf6451937267bf0d91.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "24bdfa5945fb0864c09d",
+    "revision": "cbff4df345ea66a87976",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "24bdfa5945fb0864c09d",
+    "revision": "cbff4df345ea66a87976",
     "url": "/static/js/3.js"
   },
   {
