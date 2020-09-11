3a9df51c0f13bace8aa04703aea3a11e2a965f35
Your Name <jayasoorya@crayond.co>
2020-09-11 11:47:04 +0530

    updated build file

--- build/precache-manifest.b2a7a9cdf032a734041070220109541c.js
+++ build/precache-manifest.b2a7a9cdf032a734041070220109541c.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "cbff4df345ea66a87976",
+    "revision": "7dd4c9dc6ef4ab8222da",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "cbff4df345ea66a87976",
+    "revision": "7dd4c9dc6ef4ab8222da",
     "url": "/static/js/3.js"
   },
   {
