d21a85236428897c8aa92c15a92701a2e76f6220
Your Name <jayasoorya@crayond.co>
2020-11-12 20:34:17 +0530

    -- build --

--- src/components/needHelp.js
+++ src/components/needHelp.js
@@ -207,7 +207,7 @@ export function NeedHelp(props) {
               ) : (
                 <div>
                   <div style={{ fontSize: "1rem" }}>
-                    Help
+                    Hel
                     <i
                       style={{ fontSize: "1.2rem", paddingLeft: 3 }}
                       class="fa"
