ba912ae760ad2e7ef083b7044c138bd1044fb540
prakashcrayond <prakash@crayond.co>
2020-11-21 22:06:19 +0530

    silver listing page navbar changesand detil page

--- build/precache-manifest.230dde987e6912d93ddc9bc7e3d92933.js
+++ build/precache-manifest.230dde987e6912d93ddc9bc7e3d92933.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "665d60bf04e835432ed9",
+    "revision": "0dff090453ff1fabbe21",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "665d60bf04e835432ed9",
+    "revision": "0dff090453ff1fabbe21",
     "url": "/static/js/3.js"
   },
   {
