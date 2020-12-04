3776c58cc97dd868c6d8a8a0755204e6b2134249
Your Name <jayasoorya@crayond.co>
2020-12-04 19:16:19 +0530

    updated the samir changes

--- build/precache-manifest.205d5836c69b0994bc7206b5e9ceb53f.js
+++ build/precache-manifest.205d5836c69b0994bc7206b5e9ceb53f.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "0d5cbcda8d2b7f7bd2f6",
+    "revision": "2429b5fa9a368b0a195c",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "0d5cbcda8d2b7f7bd2f6",
+    "revision": "2429b5fa9a368b0a195c",
     "url": "/static/js/3.js"
   },
   {
