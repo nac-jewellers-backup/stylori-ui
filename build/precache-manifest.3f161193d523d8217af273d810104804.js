ddd825a5423a29e1a1e71b9759cd79d42ac0d225
Your Name <jayasoorya@crayond.co>
2020-12-22 15:03:28 +0530

    updated with samirChanges branch

--- build/precache-manifest.3f161193d523d8217af273d810104804.js
+++ build/precache-manifest.3f161193d523d8217af273d810104804.js
@@ -8,11 +8,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "60bdbf781129cb841e04",
+    "revision": "5b94266c621a24369831",
     "url": "/static/css/3.css"
   },
   {
-    "revision": "85784dd56ce1ca8936b2",
+    "revision": "185767fee56faef32865",
     "url": "/static/css/main.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "60bdbf781129cb841e04",
+    "revision": "5b94266c621a24369831",
     "url": "/static/js/3.js"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "85784dd56ce1ca8936b2",
+    "revision": "185767fee56faef32865",
     "url": "/static/js/main.js"
   },
   {
