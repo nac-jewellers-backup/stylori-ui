547dbfbabfc47ec2c94f4ac1d6fe0df265367b15
Your Name <jayasoorya@crayond.co>
2021-01-18 14:19:11 +0530

    updated the payment keys

--- build/precache-manifest.6b89b31536f43ee8609ff2ef39d791a8.js
+++ build/precache-manifest.6b89b31536f43ee8609ff2ef39d791a8.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "9a138882ceec8aa8ca3a",
+    "revision": "eec13b4772f35aade835",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "9a138882ceec8aa8ca3a",
+    "revision": "eec13b4772f35aade835",
     "url": "/static/js/3.js"
   },
   {
