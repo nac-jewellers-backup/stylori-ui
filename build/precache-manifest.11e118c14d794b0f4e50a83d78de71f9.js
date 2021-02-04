de7280f16f63280c4250678db0427a082f6b6fea
Samir <samirsaji13@gmail.com>
2021-02-04 12:19:01 +0530

    banner changes

--- build/precache-manifest.11e118c14d794b0f4e50a83d78de71f9.js
+++ build/precache-manifest.11e118c14d794b0f4e50a83d78de71f9.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "c441925cf8c89a32d152",
+    "revision": "aa24e3159b80af286266",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "c441925cf8c89a32d152",
+    "revision": "aa24e3159b80af286266",
     "url": "/static/js/3.js"
   },
   {
