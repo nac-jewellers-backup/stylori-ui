058526e449b0a7b5934207ff20c0f9fc2f1587aa
Samir <samirsaji13@gmail.com>
2021-02-05 10:25:35 +0530

    changing banners

--- build/precache-manifest.a4f920b7403db57a78ad3789dfe5dfbf.js
+++ build/precache-manifest.a4f920b7403db57a78ad3789dfe5dfbf.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "aa24e3159b80af286266",
+    "revision": "242cc517a9e5106494e2",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "aa24e3159b80af286266",
+    "revision": "242cc517a9e5106494e2",
     "url": "/static/js/3.js"
   },
   {
