0fe2540163b5e9a8e4bd7951c3394b4aca80508c
Your Name <jayasoorya@crayond.co>
2021-02-22 14:55:44 +0530

    updated the banners

--- build/precache-manifest.f5db18fd11cfb3bb10bc3573797853f4.js
+++ build/precache-manifest.f5db18fd11cfb3bb10bc3573797853f4.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.css"
   },
   {
-    "revision": "04f6b24834da4e4e0f91",
+    "revision": "0b8a006a7e50352caf7b",
     "url": "/static/css/main.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.js"
   },
   {
-    "revision": "04f6b24834da4e4e0f91",
+    "revision": "0b8a006a7e50352caf7b",
     "url": "/static/js/main.js"
   },
   {
