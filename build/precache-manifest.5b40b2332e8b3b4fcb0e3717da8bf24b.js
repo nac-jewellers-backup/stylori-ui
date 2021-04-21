4d13ab806921f43ab651b41d2b8a0f023ae9aae7
Samir <samirsaji13@gmail.com>
2021-04-21 14:53:39 +0530

    Add to cart button broken fix

--- build/precache-manifest.5b40b2332e8b3b4fcb0e3717da8bf24b.js
+++ build/precache-manifest.5b40b2332e8b3b4fcb0e3717da8bf24b.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "a0b147bbbff02cef0529",
+    "revision": "4e2a4291213cb7881e1b",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "a0b147bbbff02cef0529",
+    "revision": "4e2a4291213cb7881e1b",
     "url": "/static/js/3.js"
   },
   {
