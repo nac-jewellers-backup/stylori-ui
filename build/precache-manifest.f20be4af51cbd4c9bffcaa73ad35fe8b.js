5273517c9f389e0b8b5349e050be37823ef52bee
Samir <samirsaji13@gmail.com>
2021-07-28 17:11:56 +0530

    added into gitlab

--- build/precache-manifest.f20be4af51cbd4c9bffcaa73ad35fe8b.js
+++ build/precache-manifest.f20be4af51cbd4c9bffcaa73ad35fe8b.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "6afa4cb8eeee4850bba4",
+    "revision": "a8240b19b33fefd2679f",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "6afa4cb8eeee4850bba4",
+    "revision": "a8240b19b33fefd2679f",
     "url": "/static/js/3.js"
   },
   {
