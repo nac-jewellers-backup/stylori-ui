a4cab3271483a5a603c48074720cc53727d448f7
Samir <samirsaji13@gmail.com>
2021-07-28 22:58:50 +0530

    payment fail back

--- build/precache-manifest.3ec56b34779f1595653a51b98fd3c5e2.js
+++ build/precache-manifest.3ec56b34779f1595653a51b98fd3c5e2.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "675c800dde39b2f1d3d5",
+    "revision": "4c03e5234073bb2601cd",
     "url": "/static/css/3.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.js"
   },
   {
-    "revision": "675c800dde39b2f1d3d5",
+    "revision": "4c03e5234073bb2601cd",
     "url": "/static/js/3.js"
   },
   {
--- src/screens/Checkout/paymentOption/paymentResponseFail.js
+++ src/screens/Checkout/paymentOption/paymentResponseFail.js
@@ -1,38 +1,40 @@
 import React, { useEffect, useRef } from "react";
 import { Grid, Button } from "@material-ui/core";
-import Typography from '@material-ui/core/Typography';
-import { withRouter } from 'react-router-dom';
-import CancelIcon from '@material-ui/icons/Cancel';
-import { CartContext } from 'context'
-import './payment.css';
-import cart from 'mappers/cart'
+import Typography from "@material-ui/core/Typography";
+import { withRouter } from "react-router-dom";
+import CancelIcon from "@material-ui/icons/Cancel";
+import { CartContext } from "context";
+import "./payment.css";
+import cart from "mappers/cart";
 import Footer from "components/Footer/Footer";
 import Header from "components/SilverComponents/Header";
 import Allorders from "components/accounts/allorders";
 function PaymentResponseFail(props) {
   function BackToPayment() {
-    props.history.push('/')
+    props.history.push("/");
   }
-  const handleClick = () =>{
-    
-    localStorage.setItem("panel","3")
-    props.history.push('/checkout');
-
-  }
+  const handleClick = () => {
+    localStorage.setItem("panel", "3");
+    props.history.push("/checkout");
+  };
   return (
     <>
-      <Header wishlist={props.wishlistdata}  paymentSucces={true} />
+      <Header wishlist={props.wishlistdata} paymentSucces={true} />
       <Grid container style={{ marginTop: "15px" }}>
-        <Grid item xs={12} sm={12} md={12} >
+        <Grid item xs={12} sm={12} md={12}>
           <Grid container justify="center">
             <Grid item>
               <Grid container>
                 <CancelIcon className="svgiconfail" />
-                <Typography style={{ display: "flex", alignItems: "center" }} component="h4">Sorry,Your transaction has been failed.</Typography>
+                <Typography style={{ display: "flex", alignItems: "center" }} component="h4">
+                  Sorry,Your transaction has been failed.
+                </Typography>
               </Grid>
             </Grid>
             <Grid item style={{ marginLeft: "20px" }}>
-              <Button className="retrypaymentbtn" onClick={handleClick}>Retry Payment</Button>
+              <Button className="retrypaymentbtn" onClick={handleClick}>
+                Retry Payment
+              </Button>
             </Grid>
           </Grid>
         </Grid>
@@ -40,24 +42,38 @@ function PaymentResponseFail(props) {
       </Grid>
       <Footer />
     </>
-  )
+  );
 }
 
+const Components = (props) => {
+  let {
+    CartCtx: { cartFilters, data, loading, error, allorderdata, allordersuccesful, wishlistdata },
+  } = React.useContext(CartContext);
 
-const Components = props => {
-  let { CartCtx: { cartFilters, data, loading, error, allorderdata,allordersuccesful, wishlistdata } } = React.useContext(CartContext);
-  
   let content, mapped;
   if (!loading && !error) {
     if (Object.keys(data).length !== 0) {
       mapped = cart(data);
     }
-  }
+  } 
+
+
   
-  if (Object.keys(data).length === 0) content = <div className="overall-loader"><div id="loading"></div></div>
-  else content = <PaymentResponseFail {...props} data={mapped} allorderdata={allordersuccesful} wishlistdata={wishlistdata} />
+  // else {
+  //   window.location.reload();
+  // }
+
+  // if (Object.keys(data).length === 0)
+  //   content = (
+  //     <div className="overall-loader">
+  //       <div id="loading"></div>
+  //     </div>
+  //   );
+  // else 
+  
+  content = <PaymentResponseFail {...props} data={mapped} allorderdata={allordersuccesful} wishlistdata={wishlistdata} />;
   // localStorage.setItem("a__w_l", wishlistdata && wishlistdata.length)
-  return content
-}
+  return content;
+};
 
 export default withRouter(Components);
