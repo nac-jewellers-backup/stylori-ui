fb3f5571b7d98ab75e4c1eaf8a80e7a7e6906c07
robo <samir@crayond.co>
2022-04-08 13:11:41 +0530

    merged vishnu

--- build/precache-manifest.0081b2cb0bb9c446f8aca5a3706e4043.js
+++ build/precache-manifest.0081b2cb0bb9c446f8aca5a3706e4043.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "5323c0f01aed92aa9ff8",
+    "revision": "8b139ab24a21fcd2ac52",
     "url": "/static/css/4.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.js"
   },
   {
-    "revision": "5323c0f01aed92aa9ff8",
+    "revision": "8b139ab24a21fcd2ac52",
     "url": "/static/js/4.js"
   },
   {
