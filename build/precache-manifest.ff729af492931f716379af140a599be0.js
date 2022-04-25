8dfae8240e1c829b089c7534a30e8cc31a950bfc
robo <samir@crayond.co>
2022-04-25 10:51:48 +0530

    500 error fixed

--- build/precache-manifest.ff729af492931f716379af140a599be0.js
+++ build/precache-manifest.ff729af492931f716379af140a599be0.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "085ff47f599be1e6b08c",
+    "revision": "56030d76df1ad1d27a87",
     "url": "/static/css/4.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.js"
   },
   {
-    "revision": "085ff47f599be1e6b08c",
+    "revision": "56030d76df1ad1d27a87",
     "url": "/static/js/4.js"
   },
   {
