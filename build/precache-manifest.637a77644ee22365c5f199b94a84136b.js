95d86a3834ae201727bdc473dc65438b3590fd25
robo <samir@crayond.co>
2022-05-06 11:08:33 +0530

    perfomance improved

--- build/precache-manifest.637a77644ee22365c5f199b94a84136b.js
+++ build/precache-manifest.637a77644ee22365c5f199b94a84136b.js
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.css"
   },
   {
-    "revision": "e0af0ead1e078b412c78",
+    "revision": "5b01e16a4d187aa909f7",
     "url": "/static/css/4.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.js"
   },
   {
-    "revision": "e0af0ead1e078b412c78",
+    "revision": "5b01e16a4d187aa909f7",
     "url": "/static/js/4.js"
   },
   {
