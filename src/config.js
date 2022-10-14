fd8e20e84f99ba14ef6644edf3a5e1cabc07f5e3
vardhman <vardhman@crayond.co>
2022-10-14 13:02:24 +0530

    updated GOOGLE_CLIENT_ID

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ export const API_URL = "https://api.stylori.com";
 //http://192.168.0.106:8000
 
 export const HOME_PAGE_URL = "";
-export const CDN_URL = "https://assets.stylori.com/"
+export const CDN_URL = "https://assets.stylori.com/";
 // export const CDN_URL =
 //   "https://styloriimages-staging.s3.ap-south-1.amazonaws.com/";
 // export const CDN_URL = "https://assets.stylori.com/"
 // export const CDN_URL =
 //   "https://styloriimages-staging.s3.ap-south-1.amazonaws.com/";
 
-  export const GOOGLE_CLIENT_ID =
-  "410922820159-6r6cbk3aps4icmmc53s11gh5slragib4.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID =
+  "727432939388-qmtkseqsijuge2nsarbtups82o1vslvi.apps.googleusercontent.com";
 export const FACEBOOK_APP_ID = "238023071773183";
