9bd4e2329edc39a015317dc6b47cfab5999d2ec4
renalsalex <renals@cryond.co>
2023-01-25 13:02:38 +0530

    Voucher bug has been cleared and moved to production. On 25/1/2023 by renals at 12:58pm

---
+++ build/precache-manifest.e573a45a47bab7e6a80efc1923d499aa.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747fb80d66c4acb0dba84aaf9ee7a5ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d58b99873ba647436d6",
+    "url": "/static/css/2.css"
+  },
+  {
+    "revision": "9bc831e7ab46dcfc698f",
+    "url": "/static/css/4.css"
+  },
+  {
+    "revision": "52c9c3ab37281991934e",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "9d58b99873ba647436d6",
+    "url": "/static/js/2.js"
+  },
+  {
+    "revision": "8eeb6cd75e2f991aab73",
+    "url": "/static/js/3.js"
+  },
+  {
+    "revision": "9bc831e7ab46dcfc698f",
+    "url": "/static/js/4.js"
+  },
+  {
+    "revision": "52c9c3ab37281991934e",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "265884b522a19a75c49d",
+    "url": "/static/js/runtime~main.js"
+  },
+  {
+    "revision": "895b12f026ed1d4fb5c78f2c15a4a862",
+    "url": "/static/media/Stylori-NAC-View.jpg"
+  },
+  {
+    "revision": "04639ae8969c77110a53303b92b57712",
+    "url": "/static/media/Stylorilogo.svg"
+  },
+  {
+    "revision": "599a979d7346e2fb672204304e3efbde",
+    "url": "/static/media/cartGrey.svg"
+  },
+  {
+    "revision": "7996bd6992d473ba217275885f52812f",
+    "url": "/static/media/close.svg"
+  },
+  {
+    "revision": "6b446a380e308ad3d84cf955d088f3be",
+    "url": "/static/media/facebook.svg"
+  },
+  {
+    "revision": "992ac34a80cdd509871cff78c6d98e72",
+    "url": "/static/media/gmail.svg"
+  },
+  {
+    "revision": "4bc28593bdd089d88f889e527336e18f",
+    "url": "/static/media/houseOfNAC@2x.png"
+  },
+  {
+    "revision": "1c1ce3c23e9b17a02a27aa22cf7d94d2",
+    "url": "/static/media/houseOfNAC@3x.png"
+  },
+  {
+    "revision": "28cd30ea4ad92b4520d9a3fb2634a87d",
+    "url": "/static/media/img1ProductModal.jpg"
+  },
+  {
+    "revision": "b944ac0307e92dae56a16dc18b6318a1",
+    "url": "/static/media/img2ProductModal.jpg"
+  },
+  {
+    "revision": "2e0c3b28d442f07cf1fb810420137dc5",
+    "url": "/static/media/img3ProductModal.jpg"
+  },
+  {
+    "revision": "337eef9d867ffd752a548702f05cb8e6",
+    "url": "/static/media/img4ProductModal.jpg"
+  },
+  {
+    "revision": "51fc5370cb4d7e537c06ea117f090ba2",
+    "url": "/static/media/logout.svg"
+  },
+  {
+    "revision": "f67911d2ff84fcce62033f3f3f52d654",
+    "url": "/static/media/loveGrey.svg"
+  },
+  {
+    "revision": "16dba06dc926d09764463dadfbb4c54e",
+    "url": "/static/media/percentage.svg"
+  },
+  {
+    "revision": "51ade057f1c084a5ac5034d3ed31722b",
+    "url": "/static/media/searchGrey.svg"
+  },
+  {
+    "revision": "b0c24c44cdbcd2228f0f6fef2740195f",
+    "url": "/static/media/shopping.svg"
+  },
+  {
+    "revision": "9f68d134cc7db1c64076983fcd3e8826",
+    "url": "/static/media/silverOpenLink.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.woff"
+  },
+  {
+    "revision": "18d70bcc35a56cea676c4ced2fbb3d09",
+    "url": "/static/media/stylori_silver_logo.svg"
+  },
+  {
+    "revision": "5a712650392a83e01a75498ebc83837b",
+    "url": "/static/media/telephone.svg"
+  },
+  {
+    "revision": "1737e5e0a88de55dc6cf1a773e4b0816",
+    "url": "/static/media/topPicksOne.jpg"
+  },
+  {
+    "revision": "35f5ee82b7e9b5e66f70a99aefcb3865",
+    "url": "/static/media/topPicksThree.jpg"
+  },
+  {
+    "revision": "37b89e280ecc047a56d66fdb6404bb23",
+    "url": "/static/media/topPicksTwo.jpg"
+  },
+  {
+    "revision": "c7359aa61314bf6d37f83ccc97dd0694",
+    "url": "/static/media/user-shape.svg"
+  }
+]);
